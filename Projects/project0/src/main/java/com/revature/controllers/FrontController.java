6ccd66534ee0d1728f3a18acb89f3222341b0212
Andy <56707541+atcook68@users.noreply.github.com>
2021-10-28 20:42:53 -0700

    Worked on Employee login.

--- Projects/project0/src/main/java/com/revature/controllers/FrontController.java
+++ Projects/project0/src/main/java/com/revature/controllers/FrontController.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class FrontController {
 	private static Scanner sc = new Scanner(System.in);
-	private EmployeeController ec;
-	
+	// private EmployeeController ec;
+
 	public static void runMenu() {
 		boolean run = true;
 		
@@ -26,10 +26,53 @@ public class FrontController {
 			switch(choice) {
 			
 			case"1":
-				System.out.println("Login creation");
-				System.out.println();
-				break;
+				System.out.println("Enter your username" + "\n");
+				String Input1 = "Username"; 	
+				String Input2 = "Password"; 
+				if (Input1.equals(sc.nextLine())) {
+					System.out.println(" Enter your password ");
+				}
+				if (Input2.equals(sc.nextLine())) {
+					System.out.println(" Successful login! ");
+				}
 				
+//	switch (opt) {
+
+			case "A":
+	//		MANAGER:
+	//			done = true;
+				break;
+
+			case "B":
+	//		EMPLOYEE:
+				System.out.println("Employee options:"
+						+ "1: Add an item."
+						+ "2: Remove and item."
+						+ "3: Accept an offer."
+						+ "4: Reject an offer."
+						+ "5: View payments.");
+
+				String employeeChoice = sc.nextLine();
+				switch(employeeChoice){
+					case "1": 
+						System.out.println("Add an item to the shop");
+					case "2":
+						System.out.print("Remove an item.");
+					case "3":
+						System.out.print("Accept a pending offer for an item");
+				
+					case "4":
+					System.out.print("Remove an item from the shop.");
+					case "5":
+						System.out.println("view all payments.");
+				}
+					
+					
+				
+				
+				
+				
+				break;
 			case "2":
 				// --------------- Create a selection go-to
 				System.out.println();
